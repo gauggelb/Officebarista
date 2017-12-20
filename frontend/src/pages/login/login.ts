d2e75aa77f2be7e4282a257bc05c2da937f5a81d
alinembeukem <34660269+alinembeukem@users.noreply.github.com>
2017-12-20 21:32:02 +0100

    Update login.ts

--- frontend/src/pages/login/login.ts
+++ frontend/src/pages/login/login.ts
@@ -1,3 +1,5 @@
+/*Is called with the corresponding html file*/
+
 import { Component } from '@angular/core';
 import { NavController, LoadingController, ToastController } from 'ionic-angular';
 import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
@@ -11,7 +13,7 @@ import { HomePage } from '../home/home';
 export class LoginPage {
 
   loading: any;
-  
+  /*The login expect 3 parameters but as the first one is always the same, it is set in this file*/
   userName:'';
   loginData = {strategy:'local', email:'', password:''};
   data: any;
@@ -19,10 +21,13 @@ export class LoginPage {
   tokenTransfert: any;
   
   constructor(public navCtrl: NavController, public authService: AuthServiceProvider, public loadingCtrl: LoadingController, private toastCtrl: ToastController) {}
-	
+
+  /*Call the authenticationservice and its methode login. If it is successfull, the access token the service gives back has to be saved
+  to be able to access any other data from the backend. As the same time, the new rootpage has to be set: it then becomes the homepage 
+  with multiple possibilities*/	
   doLogin() {
     this.showLoader();
-	this.getCurrentId();
+    this.getCurrentId();
     this.authService.login(this.loginData).then((result) => {
       this.loading.dismiss();
       this.data = result;
@@ -37,6 +42,7 @@ export class LoginPage {
 	
   }
 
+  /*Gets the id of the user who is logged in. It will be used to create orders or to add favorit coffee*/
   getCurrentId(){
 	this.authService.currentId(this.loginData)
 	  .subscribe(data => { 
@@ -44,7 +50,6 @@ export class LoginPage {
 		localStorage.setItem('userName',data.data[data.data.length-1].name);
 		localStorage.setItem('role',data.data[data.data.length-1].role);
 		localStorage.setItem('blocked',data.data[data.data.length-1].blocked);
-		console.log("blocked " + localStorage.getItem('blocked'));
 		});
 	}
 
