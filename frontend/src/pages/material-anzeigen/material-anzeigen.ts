38549c4b54d45c0b01c2fd13125478b9a339c4a5
alinembeukem <34660269+alinembeukem@users.noreply.github.com>
2017-12-20 21:34:20 +0100

    Update material-anzeigen.ts

--- frontend/src/pages/material-anzeigen/material-anzeigen.ts
+++ frontend/src/pages/material-anzeigen/material-anzeigen.ts
@@ -1,3 +1,5 @@
+/*Is called with the corresponding html file*/
+
 import { Component } from '@angular/core';
 import { NavController, LoadingController} from 'ionic-angular';
 import {MaterialProvider} from '../../providers/material/material'
@@ -17,15 +19,17 @@ export class MaterialAnzeigenPage {
     console.log('ionViewDidLoad MaterialAnzeigenPage');
   }
 	
-	details(){
-			this.materialService.getMaterial()
-		  .subscribe(data => { this.material= (data.data)[0];
-		  this.id =  (data.data)[0]._id;
-		  console.log(this.material);});
-		}
-
-	updateMaterial(){
-		console.log(this.id);
-		this.materialService.updateMaterial(this.id,this.material);
-	}
+/*Get the material data throught the material's service*/	
+details(){
+	this.materialService.getMaterial().subscribe(data => { 
+		this.material= (data.data)[0];
+		this.id =  (data.data)[0]._id;
+	});
+}
+	
+/*Update the material data throught the material's service*/	
+updateMaterial(){
+	console.log(this.id);
+	this.materialService.updateMaterial(this.id,this.material);
+}
 }
