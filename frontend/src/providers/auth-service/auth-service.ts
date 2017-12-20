c2330082e2097ddffe4fec8b16ebc787ebf9c910
alinembeukem <34660269+alinembeukem@users.noreply.github.com>
2017-12-20 21:42:55 +0100

    Update auth-service.ts

--- frontend/src/providers/auth-service/auth-service.ts
+++ frontend/src/providers/auth-service/auth-service.ts
@@ -11,7 +11,7 @@ export class AuthServiceProvider {
  constructor(public http: Http) {}
 
 login(credentials) {
-	var headers = new Headers();
+    var headers = new Headers();
     return new Promise((resolve, reject) => {
         headers.append('Content-Type', 'application/json');
         this.http.post(apiUrl+'/authentication', JSON.stringify(credentials), {headers:headers})
@@ -23,12 +23,13 @@ login(credentials) {
    });
  }
 
-  currentId(credentials){
-	 let headers = new Headers();
-	  headers.append('Content-Type', 'application/json');
-		headers.append('Authorization', localStorage.getItem('token'));
-      return this.http.get(apiUrl+'/users?email='+(credentials.email), {headers: headers}).map( res => res.json()); 
-  }
+currentId(credentials){
+	let headers = new Headers();
+	headers.append('Content-Type', 'application/json');
+	headers.append('Authorization', localStorage.getItem('token'));
+	return this.http.get(apiUrl+'/users?email='+(credentials.email), {headers: headers}).map( res => res.json()); 
+}
+	
   register(data) {
     return new Promise((resolve, reject) => {
         let headers = new Headers();
@@ -53,9 +54,7 @@ login(credentials) {
 	headers.append('Content-Type', 'application/json');
 	headers.append('Authorization', localStorage.getItem('token'));
 	return this.http.get(apiUrl+'/users?_id='+id , {headers: headers}).map( res => res.json());
-  }
-  
-  
+  }  
 
   getAllUsers() {  
 	var headers = new Headers();
@@ -64,19 +63,17 @@ login(credentials) {
 	return this.http.get(apiUrl+'/users' , {headers: headers}).map( res => res.json());
 	}
  
-	 updateUser(otherId, toUpdateUser){	 
-	 var headers = new Headers();
-		headers.append('Authorization', localStorage.getItem('token'));
-		headers.append('Content-Type', 'application/json');
-		console.log(JSON.stringify(toUpdateUser));
-		var url = apiUrl+'/users/'+otherId;
-		this.http.patch(url, JSON.stringify(toUpdateUser), { headers: headers })
-			.map(res => res.json())
-			.subscribe(
-				err => console.log(err)
-			);
-			
-			
-  }
- 
+updateUser(otherId, toUpdateUser){	 
+	var headers = new Headers();
+	headers.append('Authorization', localStorage.getItem('token'));
+	headers.append('Content-Type', 'application/json');
+	console.log(JSON.stringify(toUpdateUser));
+	var url = apiUrl+'/users/'+otherId;
+	this.http.patch(url, JSON.stringify(toUpdateUser), { headers: headers })
+		.map(res => res.json())
+		.subscribe(
+			err => console.log(err)
+		);			
+	}
+
 }
